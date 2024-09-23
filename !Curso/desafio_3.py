9d9c407f3bcdbe724f470f34e428e2a6d2de8349
CRUFTY <crufty01@gmail.com>
2024-09-23 20:27:01 -0300

    Update desafio_3.py

--- !Curso/desafio_3.py
+++ !Curso/desafio_3.py
@@ -1,24 +1,35 @@
+import os
+def clear_screen():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+clear_screen()
 
 diccionario = {}
 
+nombre_clave = input("Ingresá el nombre de la clave : ")
+nombre_valor = input("Ingresá el nombre del valor : ")
+
+
 while True:
-    clave = input("Ingresá un nombre (o dejá vacío para terminar): ").lower()
+    clave = input(f"Ingresá un {nombre_clave} (o dejá vacío para terminar): ").lower()
     if clave == "":
         break
-    valor = input(f"Ingresá la edad de {clave}: ")
+    valor = input(f"Ingresá la {nombre_valor} de {clave}: ").lower()
     diccionario[clave] = valor
     print("")
 
 
 for clave, valor in diccionario.items():
-    print(f"{clave}: {valor} años")
+    print(f"{clave}: {valor} {nombre_valor}")
 
 print("")
-clave_buscar = input("Ingresá el nombre que querés buscar: ").lower()
-resultado = diccionario.get(clave_buscar)
+
+clave_buscar = input(f"Ingresá el {nombre_clave} que querés buscar: ").lower()
 
 print("")
-if resultado:
-    print(f"{clave_buscar} tiene {resultado} años.")
+if clave_buscar in diccionario:
+    print(f"{clave_buscar} tiene {diccionario[clave_buscar]} {nombre_valor}.")
 else:
     print(f"{clave_buscar} no está en el diccionario.")
